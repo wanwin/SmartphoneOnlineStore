f07798c4ec2cb5d36f9fdf5cd22b4dceed018be1
wanwin <javierhamad@hotmail.com>
2016-04-05 03:10:17 +0100

    Increment and decrement product quantity in cart.jsp implemented, presentation could be improved.

--- SmartphoneOnlineStore/SmartphoneOnlineStore-war/src/java/frontController/ModifyQuantityOfProduct.java
+++ SmartphoneOnlineStore/SmartphoneOnlineStore-war/src/java/frontController/ModifyQuantityOfProduct.java
@@ -1,14 +1,42 @@
 package frontController;
 
+import entity.Product;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+
 public class ModifyQuantityOfProduct extends FrontCommand{
 
     @Override
     public void process() {
-        if (request.getParameter("increment") != null){
-            
-        }    
-        if (request.getParameter("decrement") != null){
-            
-        }    
+        try {
+            HashMap<Product,Integer> products = initCart().getProducts();
+            String requestedProductId = request.getParameter("productId");
+            if (request.getParameter("incrementButton") != null){
+                for (Entry<Product,Integer> entry : products.entrySet()) {
+                    Product product = entry.getKey();
+                    Integer quantity = entry.getValue();
+                    if (product.getProductId() == Integer.parseInt(requestedProductId)){
+                        products.put(product, quantity + 1);
+                    }
+                }
+            }
+            if (request.getParameter("decrementButton") != null){
+                for (Entry<Product,Integer> entry : products.entrySet()) {
+                    Product product = entry.getKey();
+                    Integer quantity = entry.getValue();
+                    if (product.getProductId() == Integer.parseInt(requestedProductId) && (quantity - 1) >= 1){
+                        products.put(product, quantity - 1);
+                    }
+                }    
+            }    
+            try {
+                forward(CART_PATH);
+            } catch (ServletException | IOException ex) {
+            }
+        } catch (NamingException ex) {
+        }
     }
 }
